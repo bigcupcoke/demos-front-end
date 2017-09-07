b99ae0b614f9944e29fd7e5b5d2a86b0da618e1e
bigcupcoke <dujun.sc@outlook.com>
2017-09-07 22:47:56 +0800

    tomato modal

---
+++ tomato/static/js/config.js
@@ -0,0 +1,15 @@
+const config = {
+    tomato: {
+        count: 1,
+        perMins: {
+            task: 30,
+            work: 25,
+            rest: 5,
+        },
+        allMins: {
+            task: 30,
+            work: 25,
+            rest: 5,
+        },
+    }
+}
--- tomato/static/js/tomato.js
+++ tomato/static/js/tomato.js
@@ -2,15 +2,22 @@ const log = console.log.bind(console)
 
 class Modal {
     constructor() {
-        // this.init()
+        this.init()
     }
 
-    static init() {
+    static new() {
+        const instance = new this()
+        config.modal = instance
+        return instance
+    }
+
+    init() {
         var t = this.template()
         $('body').append(t)
+        this.bindEvents()
     }
 
-    static template() {
+    template() {
         var t = `
         <div class="modal fade center" id="modal-add" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
              data-backdrop="true"
@@ -37,24 +44,24 @@ class Modal {
                                     3个
                                 </div>
                                 <div class="col-sm-6 text-right" style="line-height: 35px;">
-                                    <button type="button" class="btn btn-danger" ng-click="minusTomato()"><span
+                                    <button type="button" class="btn btn-danger btn-tomato-count-decrease"><span
                                             class="glyphicon glyphicon-minus"></span></button>
                                     &nbsp;&nbsp;
-                                    <button type="button" class="btn btn-primary" ng-click="addTomato()"><span
+                                    <button type="button" class="btn btn-primary btn-tomato-count-increase"><span
                                             class="glyphicon glyphicon-plus"></span></button>
                                 </div>
                             </div>
                         </form>
                         </br>
                         <div class="form-group">
-                            <span class="col-sm-4">{{totalTime}}</span>
-                            <span class="col-sm-4">{{taskTime}}</span>
-                            <span class="col-sm-4">{{restTime}}</span>
+                            <span class="col-sm-4">总共时间</span>
+                            <span class="col-sm-4">任务时间</span>
+                            <span class="col-sm-4">休息时间</span>
                         </div>
                         <div class="form-group">
-                            <h5 class="col-sm-4">{{t_time}}</h5>
-                            <h5 class="col-sm-4">{{w_time}}</h5>
-                            <h5 class="col-sm-4">{{r_time}}</h5>
+                            <h5 class="col-sm-4 task-time">{{t_time}}</h5>
+                            <h5 class="col-sm-4 work-time">{{w_time}}</h5>
+                            <h5 class="col-sm-4 rest-time">{{r_time}}</h5>
                         </div>
                         </br>
                     </div>
@@ -72,9 +79,74 @@ class Modal {
         return t
     }
 
-    static show() {
+    show() {
         log('show')
         $('#modal-add').modal('show')
+    }
+
+    bindEvents() {
+        var _t = this
+        _t.bindEventBtn()
+        _t.bindEventUpDown()
+        // _t.foucsOnInput()
+    }
+
+    bindEventBtn() {
+        $('body').on('click', '.btn-tomato-count-increase', () => {
+            this.increase()
+        })
+
+        $('body').on('click', '.btn-tomato-count-decrease', () => {
+            this.decrease()
+        })
+    }
+
+    bindEventAccept() {
+        $('body'.on('click'), '.button-accept', () => {})
+    }
+
+    increase() {
+        this.changeCount(1)
+    }
+
+    decrease() {
+        this.changeCount(-1)
+    }
+
+    changeCount(offest) {
+        config.tomato.count += offest
+        $('.estimate-count').text(`${config.tomato.count} 个`)
+        let items = Object.keys(config.tomato.perMins)
+        items.forEach((e) => this.changeTimeText(e))
+    }
+
+    changeTimeText(item) {
+        // log('item', item)
+        const t = config.tomato
+        const mAll = t.perMins[item] * t.count
+        const hour = Math.floor(mAll / 60)
+        const min = mAll - hour * 60
+        // log(mAll, hour, min)
+        let r =  `${hour}小时${min}分钟`
+        $(`.${item}-time`).text(r)
+    }
+
+    foucsOnInput() {
+        $('.form-control').foucs()
+    }
+
+    bindEventUpDown() {
+        const _t = this
+        $('body').on('keydown', '.form-control', function(e) {
+            let key = e.key
+
+            const o = {
+                'ArrowUp': _t.increase,
+                'ArrowDown': _t.decrease,
+            }
+            // log('key', key, o[key])
+            o[key].call(_t)
+        })
     }
 }
 
@@ -95,12 +167,31 @@ class Tomato {
         log('bind')
         $('body').on('click', '#tomato-add', () => {
             log('click')
-            Modal.init()
-            Modal.show()
+            let m = Modal.new()
+            m.show()
+            // Modal.show()
         })
     }
+}
 
-    // add() {
-    //     this.modal.show()
-    // }
+class Todo {
+    constructor(form={}) {
+        this.title = from.title || '未命名'
+        this.count = from.count || 0
+        this.status = 'paused'
+        this.interuppet = 0
+        this.init()
+    }
+
+    static new() {
+
+    }
+
+    init() {
+
+    }
+
+    template() {
+
+    }
 }
